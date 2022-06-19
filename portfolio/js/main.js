138f8a3aeb86374271466d7e3832b8077a03aa8a
sigfriedga <sigfriedga@hotmail.com>
2022-06-19 12:01:10 +0200

    Toggle the navbar menu open/close

---
+++ portfolio/js/main.js
@@ -0,0 +1,10 @@
+//Get the Elements
+const navbar = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+//Toggle the navbar menu on click Open/Close
+menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle('nav-background');
+    menu.classList.toggle('nav-toggle');
+})
